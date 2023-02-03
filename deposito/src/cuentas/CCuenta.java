c5eb1e6a226485d3033fdfd69ec0e5f42e487e63
Ziuni85 <david_bd_85@hotmail.com>
2023-02-03 20:26:10 +0100

    mi primer commit.

--- deposito/src/cuentas/CCuenta.java
+++ deposito/src/cuentas/CCuenta.java
@@ -1,127 +1,175 @@
 package cuentas;
 
-
 /**
- * @author david
+ * 
+ * 
+ * @author David Blanco Domínguez.
  *
  */
 public class CCuenta {
 
+	
+	private String nombre;
+	private String cuenta;
+	private double saldo;
+	private double tipoInterés;
 
-    private String nombre;
-    private String cuenta;
-    private double saldo;
-    private double tipoInterés;
-
-    public CCuenta()
-    {
-    }
-
-    public CCuenta(String nom, String cue, double sal, double tipo)
-    {
-        nombre =nom;
-        cuenta=cue;
-        saldo=sal;
-    }
-    
+	/**
+	 * Constructor vacío.
+	 */
+	public CCuenta() {
+	}
 /**
- * 
- * @return
+ * Constructor con parámetros.
+ * @param nom
+ * @param cue
+ * @param sal
+ * @param tipo
  */
-    public String getNombre() {
+	public CCuenta(String nom, String cue, double sal, double tipo) {
+		nombre = nom;
+		cuenta = cue;
+		saldo = sal;
+	}
+
+	/**
+	 * 
+	 * 
+	 * @return atributo nombre
+	 */
+	public String getNombre() {
 		return nombre;
 	}
 
-    /**
-     * 
-     * @param nombre
-     */
+	/**
+	 *Cambia el valor del atributo nombre.
+	 * 
+	 * @param nombre
+	 */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
 
 	/**
 	 * 
-	 * @return
+	 * 
+	 * @return atributo cuenta
 	 */
 	public String getCuenta() {
 		return cuenta;
 	}
 
+	/**
+	 * Cambia el valor del atributo cuenta.
+	 * 
+	 * @param cuenta
+	 */
 	public void setCuenta(String cuenta) {
 		this.cuenta = cuenta;
 	}
 
-	 public double estado()
-	    {
-	        return saldo;
-	    }
+	/**
+	 * 
+	 * 
+	 * 
+	 * @return saldo
+	 */
+	public double estado() {
+		return saldo;
+	}
 
+	/**
+	 * Cambia el valor del atributo saldo.
+	 * 
+	 * @param saldo
+	 */
 	public void setSaldo(double saldo) {
 		this.saldo = saldo;
 	}
 
+	/**
+	 * 
+	 * @return  atributo tipoInteres.
+	 */
 	public double getTipoInterés() {
 		return tipoInterés;
 	}
 
+	
+	/**
+	 * Cambia el valor del atributo tipoInterés.
+	 * @param tipoInterés
+	 */
 	public void setTipoInterés(double tipoInterés) {
 		this.tipoInterés = tipoInterés;
 	}
+	/**
+	 * Ingresa una cantidad mayor que cero.
+	 * @param cantidad
+	 * @throws Exception
+	 */
+	public void ingresar(double cantidad) throws Exception {
+		if (cantidad < 0)
+			throw new Exception("No se puede ingresar una cantidad negativa");
+		saldo = saldo + cantidad;
+	}
+/**
+ * Retira el parámetro cantidad de la cuenta si este es válido.
+ * @param cantidad
+ * @throws Exception
+ */
+	public void retirar(double cantidad) throws Exception {
+		if (cantidad <= 0)
+			throw new Exception("No se puede retirar una cantidad negativa");
+		if (estado() < cantidad)
+			throw new Exception("No se hay suficiente saldo");
+		saldo = saldo - cantidad;
+	}
+/**
+ * Retira una cantidad de 2.300 e ingresa una de 695.
+ * @throws Exception
+ */
+	public void operativa_cuenta() throws Exception {
+		double saldoActual;
 
-	public void ingresar(double cantidad) throws Exception
-    {
-        if (cantidad<0)
-            throw new Exception("No se puede ingresar una cantidad negativa");
-        saldo = saldo + cantidad;
-    }
+		saldoActual = this.estado();
+		System.out.println("El saldo actual es" + saldoActual);
 
-    public void retirar(double cantidad) throws Exception
-    {
-        if (cantidad <= 0)
-            throw new Exception ("No se puede retirar una cantidad negativa");
-        if (estado()< cantidad)
-            throw new Exception ("No se hay suficiente saldo");
-        saldo = saldo - cantidad;
-    }
-    
-    public void operativa_cuenta() throws Exception
-    {
-        double saldoActual;
+		try {
+			this.retirar(2300);
+		} catch (Exception e) {
+			System.out.print("Fallo al retirar");
+		}
+		try {
+			System.out.println("Ingreso en cuenta");
+			this.ingresar(695);
+		} catch (Exception e) {
+			System.out.print("Fallo al ingresar");
+		}
 
-        saldoActual = this.estado();
-        System.out.println("El saldo actual es"+ saldoActual );
+	}
+/**
+ * Retira el parametro introducido como cantidad y despues lo vuelve a ingresar. 
+ * @param cantidad
+ * @throws Exception
+ */
+	public void operativa_cuenta(float cantidad) throws Exception {
+		System.out.println("El saldo actual es" + this.estado());
 
-        try {
-        	this.retirar(2300);
-        } catch (Exception e) {
-            System.out.print("Fallo al retirar");
-        }
-        try {
-            System.out.println("Ingreso en cuenta");
-            this.ingresar(695);
-        } catch (Exception e) {
-            System.out.print("Fallo al ingresar");
-        }
-        
-    }
-    
-    public void operativa_cuenta(float cantidad) throws Exception
-    {
-        System.out.println("El saldo actual es"+ this.estado() );
+		try {
+			this.retirar(cantidad);
+		} catch (Exception e) {
+			System.out.print("Fallo al retirar");
+		}
+		try {
+			System.out.println("Ingreso en cuenta");
+			this.ingresar(cantidad);
+		} catch (Exception e) {
+			System.out.print("Fallo al ingresar");
+		}
 
-        try {
-        	this.retirar(cantidad);
-        } catch (Exception e) {
-            System.out.print("Fallo al retirar");
-        }
-        try {
-            System.out.println("Ingreso en cuenta");
-            this.ingresar(cantidad);
-        } catch (Exception e) {
-            System.out.print("Fallo al ingresar");
-        }
-        
-    }
+	}
+	
+	}
 
-}
+
--- deposito/src/cuentas/Main.java
+++ deposito/src/cuentas/Main.java
@@ -9,14 +9,15 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
     	//Se crea el objeto cuenta1 al que hemos refactorizado (antes miCuenta)
-        CCuenta cuenta1;
+        CCuenta cuenta1 =  new CCuenta();
+
         //Construimos el objeto con un constructor con paso de parámetros para inicializar los atributos
         cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
-        //Llamamos al método operativa_cuenta que engloba las líneas de código mas abajo comentadas al final de la clase main
-        cuenta1.operativa_cuenta();
-      //Llamamos al método operativa_cuenta con parametro cantidad que engloba las líneas de código mas abajo comentadas al final de la clase main
-        cuenta1.operativa_cuenta(500);
-        System.out.print( cuenta1.estado());
+//        //Llamamos al método operativa_cuenta que engloba las líneas de código mas abajo comentadas al final de la clase main
+       cuenta1.operativa_cuenta();
+//      //Llamamos al método operativa_cuenta con parametro cantidad que engloba las líneas de código mas abajo comentadas al final de la clase main
+//        cuenta1.operativa_cuenta(500);
+//        System.out.print( cuenta1.estado());
         
           
         //CODIGO ANTIGUO COMENTADO
@@ -38,5 +39,6 @@ public class Main {
 //        } catch (Exception e) {
 //            System.out.print("Fallo al ingresar");
 //        }
+        
     }
 }
